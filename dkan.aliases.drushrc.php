bb2b6a99a127234a00ddc4841495d1a14c772692
Nansen <webadmin@nansen.nuamsdev.com>
2014-12-17 14:41:50 +0000

    Updates for using script

---
+++ dkan.aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+$aliases['local'] = array(
+  'makefile' => 'build-dkan.make',
+  'profile' => 'dkan',
+  'site-name' => 'DKAN',
+  'git' => array(
+    'url' => 'git@github.com:NuCivic/dkan.git',
+    'branch' => '7.x-1.x',
+  ),
+);
