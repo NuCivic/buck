60e9f37d86d109d5a46e1fb164a8a962de08d734
dkinzer <dtkinzer@gmail.com>
2015-08-26 13:25:13 +0000

    Add stop tests.
    Trying to get this to work with
    
    `phpunit --configuration tests`
    
    But still having a little bit of trouble with autoload of Unish stuff.

---
+++ tests/BuckUnitTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Buck Unit tests.
+ */
+
+namespace Unish;
+
+/**
+ * Class  BuckPermTest.
+ *
+ * @group commands
+ */
+class BuckPermTest extends CommandUnishTestCase {
+}
